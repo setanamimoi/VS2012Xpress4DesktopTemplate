24966183d3c1034fbcd3d50df32498876484fd7d
setanamimoi <sgmchuli@gmail.com>
2012-11-17 11:47:59 +0900

    エントリポイントの作成

---
+++ _Tests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _Tests
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main(string[] arguments)
+        {
+        }
+    }
+}
